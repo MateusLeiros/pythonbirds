563fd996224ad84e7ac06946cd4eb513035afe8e
Mateus Leiros <mateusleiros@gmail.com>
2022-02-02 10:33:54 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,13 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 10):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá {self.nome}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Mateus', 27)
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
