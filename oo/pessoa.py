a427b19b5ebe6f716bf14d2e24aeba97c6e9562f
Mateus Leiros <mateusleiros@gmail.com>
2022-02-03 14:33:20 -0300

    Criado atributo de classe 'olhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 10):
         self.nome = nome
         self.idade = idade
@@ -17,7 +19,10 @@ if __name__ == '__main__':
     mateus.sobrenome = 'Leiros'
     for filho in lucimar.filhos:
         print(filho.nome)
+    #__dict__ possui referencias apenas à atributos de instância, não apresenta atributos de classe, exceto no caso de uma alteração dinâmica
     print(lucimar.__dict__)
     print(mateus.__dict__)
     del mateus.sobrenome
     print(mateus.__dict__)
+    print(Pessoa.olhos)
+    print(mateus.olhos)
